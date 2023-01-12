f773a97eab4eef3db736a3ce6f75956974a2c936
Алексей Тапехин <Aleksey.Tapekhin@vittepro.ru>
2023-01-12 09:32:18 +0300

    Выведены 2 точки на экран Лекция2

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!!!");
+Console.SetWindowSize(40, 30);
+Console.SetBufferSize(40,30);
+
+
+Drow(2, 3, '*');
+Drow(5, 10, '#');
+
+Console.ReadLine();      
+
+static void Drow(int x, int y, char c)
+{
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine(c);
+}
