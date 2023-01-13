e126a0ca89e5eb724c04cfcc20056f5e9014056a
Алексей Тапехин <Aleksey.Tapekhin@vittepro.ru>
2023-01-13 10:51:49 +0300

    Посмотрел как работает rnd

---
+++ Tetris/Temp/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+Random rnd = new Random();
+//int k = rnd.Next(0, 5);
+
+for (int i = 0; i < 20; i++)
+{
+    Console.WriteLine(rnd.Next(0, i));
+}
+
+
